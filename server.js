be76de487f90958901b45a153a6c0e3f8e23d07e
Dmitriy Cherenkov <dimacherenkov97@gmail.com>
2021-11-29 19:12:35 +0200

    first commit

--- server.js
+++ server.js
@@ -1,37 +1,63 @@
-const express = require('express')
-const faker = require('faker')
-const app = express()
-const port = 3001
+const express = require('express');
+const faker = require('faker');
+const app = express();
+const port = 3001;
+const cors = require('cors');
+const mongoose = require('mongoose');
+ 
+mongoose.connect('mongodb://localhost/my-react-app');
+
+const Schema = mongoose.Schema;
+
+const Product = new Schema({
+  name: String,
+  breed: String,
+  image: String
+});
+
+const ProductModel = mongoose.model('Card', Product);
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
-const generateDogs = () => {
-  const dogs = [];
-  for(let i = 0; i < 100; i++) {
-    dogs.push({
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName(),
-      breed: faker.animal.dog(),
-      color: faker.commerce.color(),  
-      image: faker.image.animals(),
-      country: faker.address.country(),
-      address: faker.address.timeZone(),
-      price: faker.commerce.price(),
-      desc: faker.lorem.words(),
-      phone: faker.phone.phoneNumber('+380#########')
-    })
-  }
-  return {data: dogs}
-}
-
-let dataObj = generateDogs();
+app.get('/products', (req, res) => {
+  ProductModel.find({}, function(err, products) {
+    res.send(products)
+  });
+});
 
 
-
-app.get('/', (req, res) => {
-  res.send(dataObj)
+app.get('/products/:id', (req, res) => {
+  let result = forCards.find((item) => item.id === req.params.id)
+  res.send(result)
 })
 
-console.log(dataObj)
+
+app.post('/products',(req, res) => {
+  let dataPost = req.body;
+const instance = new ProductModel(dataPost);
+instance.save(function (err) {
+  console.log(err)
+});
+res.send('ok')
+  });
+
+
+  app.delete('/products/:id',(req, res) => {
+    console.log('wqew')
+    ProductModel.deleteOne({_id:  req.params.id}).then(() => {
+      res.send('okey')
+    })
+    .catch((e) => {
+      console.log(e)
+      res.status(400).send('good')
+    })
+  });
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
