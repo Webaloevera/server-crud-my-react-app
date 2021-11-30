56b705964e7705227c4b9371c409358a50973abc
Dmitriy Cherenkov <dimacherenkov97@gmail.com>
2021-11-30 19:03:12 +0200

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const faker = require('faker');
 const cors = require('cors');
 const mongoose = require('mongoose');
  
@@ -11,14 +12,39 @@ const Schema = mongoose.Schema;
 const Product = new Schema({
   name: String,
   breed: String,
-  image: String
+  image: String,
+  color: String,
+  country: String,
+  address: String,
+  price: String,
+  desc: String,
+  phone: String,
 });
+
 
 const ProductModel = mongoose.model('Card', Product);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// const createData = () => {
+//   for(let i = 0; i < 5000; i++) {
+//     new ProductModel({
+//       name: faker.name.firstName(),
+//       breed: faker.animal.dog(),
+//       color: faker.commerce.color(),  
+//       image: faker.image.animals(),
+//       country: faker.address.country(),
+//       address: faker.address.timeZone(),
+//       price: faker.commerce.price(),
+//       desc: faker.lorem.words(),
+//       phone: faker.phone.phoneNumber('+380#########')
+//     }).save();
+//   }
+// }
+// createData();
+
 
 
 app.get('/products', (req, res) => {
@@ -28,10 +54,17 @@ app.get('/products', (req, res) => {
 });
 
 
-// app.get('/products/:id', (req, res) => {
-//   let result = forCards.find((item) => item.id === req.params.id)
-//   res.send(result)
-// })
+app.get('/product/:id', (req, res) => {
+  ProductModel.findOne({_id: req.params.id}, req.body, (err, product) => {
+    res.send(product)
+  });
+})
+
+app.get('/products/:id/edit', (req, res) => {
+  ProductModel.findOne({_id: req.params.id}, req.body, (err, product) => {
+    res.send(product)
+  });
+})
 
 
 app.post('/products',(req, res) => {
@@ -46,7 +79,7 @@ res.send('ok')
   
 app.put('/products/:id',(req, res) => {
     let dataPut = req.body;
-  ProductModel.findOneAndUpdate({_id:req.params.id}, dataPut,  (error, doc) => {
+  ProductModel.findOneAndUpdate({_id: req.params.id}, dataPut,  (error, doc) => {
     console.log(error)
   });
   res.send('ok')
@@ -54,7 +87,7 @@ app.put('/products/:id',(req, res) => {
 
 
   app.delete('/products/:id',(req, res) => {
-    ProductModel.deleteOne({_id:  req.params.id}).then(() => {
+    ProductModel.deleteOne({_id: req.params.id}).then(() => {
       res.send('okey')
     })
     .catch((e) => {
@@ -62,8 +95,6 @@ app.put('/products/:id',(req, res) => {
       res.status(400).send('good')
     })
   });
-
-
 
 
 app.listen(port, () => {
